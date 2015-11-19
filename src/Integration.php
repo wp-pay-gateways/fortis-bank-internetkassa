04a18a283dc425a8c82e8f791dd12281d0e411cb
Remco Tolsma <info@remcotolsma.nl>
2015-11-19 12:25:47 +0100

    Extend from Ogone library.

--- src/Integration.php
+++ src/Integration.php
@@ -1,6 +1,6 @@
 <?php
 
-class Pronamic_WP_Pay_Gateways_FortisBank_IDealInternetKassa_Integration {
+class Pronamic_WP_Pay_Gateways_FortisBank_IDealInternetKassa_Integration extends Pronamic_WP_Pay_Gateways_Ogone_OrderStandard_Integration {
 	public function __construct() {
 		$this->id         = 'fortis-bank-internetkassa';
 		$this->name       = 'Fortis Bank - iDEAL Internet Kassa';
