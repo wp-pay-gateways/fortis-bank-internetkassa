066a7c4de3fac02eed4fefee4b70dd4a00384478
Remco Tolsma <info@remcotolsma.nl>
2016-01-04 14:26:15 +0100

    Fixed class name.

--- src/ConfigFactory.php
+++ src/ConfigFactory.php
@@ -10,6 +10,6 @@
  */
 class Pronamic_WP_Pay_Gateways_FortisBank_IDealInternetKassa_ConfigFactory extends Pronamic_WP_Pay_Gateways_Ogone_OrderStandard_ConfigFactory {
 	public function __construct() {
-		parent::__construct( 'Pronamic_WP_Pay_Gateways_FortisBank_IDealInternetKassa_Config', 'Pronamic_WP_Pay_Gateways_FortisBank_IDealInternetKassa_ConfigTest' );
+		parent::__construct( 'Pronamic_WP_Pay_Gateways_FortisBank_IDealInternetKassa_Config', 'Pronamic_WP_Pay_Gateways_FortisBank_IDealInternetKassa_TestConfig' );
 	}
 }
